1ada8e96e7b04b606659dd11d73e45c30922a0bb
pingsing <piosio990@gmail.com>
2022-10-19 18:24:43 +0900

    2022-10-19 18:24

--- scottpractice.sql
+++ scottpractice.sql
@@ -46,7 +46,7 @@ order by sal asc;   --오름차순 정렬
 
 select *
 from emp
-order by sal asc;   --내림차순 정렬
+order by sal desc;   --내림차순 정렬
 
 select *
 from emp
@@ -255,7 +255,7 @@ WHERE deptno = 30 AND sal>2000;
 --4번문제
 SELECT empno, ename, job, mgr, hiredate, sal, comm, deptno
 FROM emp
-WHERE sal<2000 or sal>3000;
+WHERE sal<2000 OR sal>3000;
 
 --5번문제
 SELECT ename, empno, sal, deptno
@@ -333,10 +333,10 @@ SELECT
 FROM dual;
 
 SELECT
-    ROUND(1234.5678),
-    ROUND(1234.5678,0),
-    ROUND(1234.5678,1),
-    ROUND(1234.5678,2)
+    TRUNC(1234.5678),
+    TRUNC(1234.5678,0),
+    TRUNC(1234.5678,1),
+    TRUNC(1234.5678,2)
 FROM dual;  
 
 SELECT
@@ -352,3 +352,257 @@ FROM dual;
 SELECT *
 FROM emp
 WHERE MOD(empno, 2) = 1;
+
+--날짜 데이터를 다루는 날짜 함수
+--오늘
+select sysdate
+from dual;
+
+--어제
+select sysdate -1
+from dual;
+
+--내일
+select sysdate +1
+from dual;
+
+--차이가 일수 반환
+select sysdate - hiredate as 근무일수
+from emp;
+
+--근속년수
+select trunc((sysdate-hiredate)/365) as 근무일수
+from emp;
+--포맷 모델
+--CC: 네 자리 연도의 끝 두 자리를 기준으로 사용(2022년은 2050년 이하이므로 반올림 할 경우 2001년으로 처리).
+--YYYY: 날짜 데이터의 해당 연, 월, 일의 7월 1일을 기준(2022년 10월 19일 일 경우, 2023년으로 처리).
+--Q: 각 분기의 두 번째 달의 16일 기준(아직 11월 15일이 넘지 않았으므로 22년 10월 1일).
+--DDD: 해당 일의 정오(12:00:00)를 기준(정오가 넘지 않았으므로 22년 10월 19일).
+--HH: 해당일의 시간을 기준(22년 10월 19일 10:49 이므로 반올림해도 22년 10월 19일).
+select sysdate,
+    round(sysdate, 'CC') as format_CC,
+    round(sysdate, 'YYYY') as format_YYYY,
+    round(sysdate, 'Q') as format_Q,
+    round(sysdate, 'DDD') as format_DDD,
+    round(sysdate, 'HH') as format_HH  
+from dual;
+
+select sysdate,
+    trunc(sysdate, 'CC') as format_CC,
+    trunc(sysdate, 'YYYY') as format_YYYY,
+    trunc(sysdate, 'Q') as format_Q,
+    trunc(sysdate, 'DDD') as format_DDD,
+    trunc(sysdate, 'HH') as format_HH  
+from dual;
+
+--자료형변환 함수
+--to_char()
+--to_number()
+--to_date()
+
+--날짜를 문자
+select sysdate, to_char(sysdate, 'YYYY-MM-DD HH24:MI:SS') as 현재시간
+from dual;
+
+select hiredate, to_char(hiredate, 'YYYY-MM-DD HH24:MI:SS') as 입사일자
+from emp;
+
+--숫자를 문자
+select to_char(123456, '999,999')
+from dual;
+
+select to_char(123456, 'L999,999')
+from dual;
+
+select sal, to_char(sal, 'L999,999')
+from emp;
+
+--문자를 숫자로
+select '20000' - 10000
+from dual;
+
+select 20000 - '10000'
+from dual;
+
+select '20000' - '5000'
+from dual;
+
+--to_number() 형변환을 해야한다.
+select '20,000' - '5,000'
+from dual;
+
+select to_number('20,000','999,999') - to_number('5,000','999,999')
+from dual;
+
+--문자를 날짜로
+select to_date('20221019','YYYY/MM/DD')
+from dual;
+
+select *
+from emp
+where hiredate < to_date('19820101','YYYY-MM-DD');
+
+--null데이터 처리
+--nvl(null, 바꾸고싶은 데이터)
+--nvl은 null 데이터에 타입과 같은 타입을 변경해야 한다.
+--nvl(숫자,숫자), nvl(문자,문자)
+select ename 사원명,sal,sal*12 + nvl(comm,0) as 연봉,comm
+from emp;
+
+select *
+from emp
+where mgr is null;
+
+select ename, job, mgr
+from emp
+where mgr is null;
+
+select ename, job, nvl(mgr,'CEO') as mgr
+from emp
+where mgr is null;
+
+--ORA-01722: 수치가 부적합합니다
+--01722. 00000 -  "invalid number"
+--*Cause:    The specified number was invalid.
+--*Action:   Specify a valid number.
+
+select ename, job, nvl(to_char(mgr,'9999'),'CEO') as mgr
+from emp
+where mgr is null;
+
+select comm, nvl2(comm,'O','X')
+from emp;
+
+--조건문 표현 함수
+--decode()  → switch
+--case  → if
+select ename,job,deptno,
+decode(deptno, 10,'AAA',20,'BBB',30,'CCC','기타') as 부서명
+from emp;
+
+-- 범위를 조건으로 설정할 수 있다.
+case
+    when 조건식 then 실행문
+    when 조건식 then 실행문
+    when 조건식 then 실행문
+    else 실행문
+end as 별칭
+
+select ename,job,deptno,
+    case
+    when deptno = 10 then 'AAA'
+    when deptno = 20 then 'BBB'
+    when deptno = 30 then 'CCC'
+    else '기타'
+end as 부서명
+from emp;
+
+select ename, job, sal,
+case
+    when sal between 3000 and 5000 then '임원'
+    when sal >= 2000 and sal < 3000 then '관리자'
+    when sal > 500 and sal < 2000 then '사원'
+    else '기타'
+end as 직무
+from emp;
+
+--문제풀기
+--Q1
+select empno, rpad(substr(empno,1,2),4,'*') as MASKING_EMPNO, ename, rpad(substr(ename, 1,1),5,'*') as MASKING_ENAME
+from emp
+where length(ename) >= 5 and length(ename) < 6;
+
+--Q2
+select empno, ename, sal, trunc(sal / 21.5,2) as DAY_PAY, round(((sal / 21.5)/8),1) TIME_PAY
+from emp;
+
+-Q3
+select empno, ename, mgr,
+case
+    when mgr is null then '0000'
+    when substr(mgr, 1, 2) = 75 then '5555'
+    when substr(mgr, 1, 2) = 76 then '6666'
+    when substr(mgr, 1, 2) = 77 then '7777'
+    when substr(mgr, 1, 2) = 78 then '8888'
+    else to_char(mgr)
+end as CHG_MGR
+from emp;
+
+select sum(sal)
+from emp;
+
+select avg(sal)
+from emp;
+
+select count(*), count(comm)
+from emp;
+
+select max(sal), min(sal)
+from emp;
+
+--불가능
+select ename, max(sal)
+from emp;
+
+select max(sal)
+from emp;
+
+--sum(),avg(),count(),max(),min()
+--일반컬럼과 같이 사용 불가
+--크기비교가 가능한 모든 타입에 사용 가능하다.
+
+--select 컬럼명
+--from 테이블명
+--where 조건식
+--group by 기준컬럼
+--order by 컬럼명 정렬방식 → 맨 마지막에 작성
+
+select avg(sal), 10 as deptno
+from emp
+where deptno = 10 
+UNION ALL
+select avg(sal), 20 as deptno
+from emp
+where deptno = 20
+UNION ALL
+select avg(sal), 30 as deptno
+from emp
+where deptno = 30;
+
+select deptno
+from emp
+group by deptno;
+
+select avg(sal)
+from emp
+group by deptno;
+
+select deptno, avg(sal)
+from emp
+group by deptno;
+
+select deptno, job, avg(sal)
+from emp
+group by deptno, job
+order by deptno, job;
+
+--select 컬럼명
+--from 테이블명
+--where 조건식
+--group by 기준컬럼
+--having 조건식
+--order by 컬럼명 정렬방식 → 맨 마지막에 작성
+
+select deptno, avg(sal)
+from emp
+group by deptno
+having avg(sal) >= 2000;
+--group by 에 의해서 조회 결과에 조건을 준다
+--조건식을 작성할 때 그룹함수를 사용한다.
+
+select deptno, avg(sal)
+from emp
+where deptno != 10
+group by deptno
+having avg(sal) >= 2000;
+
